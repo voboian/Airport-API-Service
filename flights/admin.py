9d812e7c440c4701c67ac475121c758e6620c63f
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-30 13:24:10 +0200

    create User model and add all models in admin panel

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from flights.models import (
+    Airport,
+    AirplaneType,
+    Airplane,
+    Route,
+    Crew,
+    Flight,
+    Order,
+    Ticket
+)
+
+admin.site.register(Airport)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)
